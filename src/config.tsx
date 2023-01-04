2a240eb0dfda641e22833cebbfc38ef4e8792dde
Jon Rutter <contact@jonrutter.io>
2023-01-04 18:01:45 -0500

    Create site config
    - Export social links array

---
+++ src/config.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { GitHubIcon } from './components/icons/GitHub';
+import { LinkedinIcon } from './components/icons/Linkedin';
+import { CodePenIcon } from './components/icons/CodePen';
+import { MastodonIcon } from './components/icons/Mastodon';
+
+type SocialLink = {
+  label: string;
+  icon: React.ReactNode;
+  url: string;
+};
+
+export const socialLinks: SocialLink[] = [
+  {
+    label: 'GitHub',
+    icon: <GitHubIcon className="w-5 h-5" />,
+    url: 'https://github.com/jonrutter',
+  },
+  {
+    label: 'Linkedin',
+    icon: <LinkedinIcon className="w-5 h-5" />,
+    url: 'https://www.linkedin.com/in/rutterjt/',
+  },
+  {
+    label: 'CodePen',
+    icon: <CodePenIcon className="w-5 h-5" />,
+    url: 'https://codepen.io/jonrutter',
+  },
+  {
+    label: 'Mastodon',
+    icon: <MastodonIcon className="w-5 h-5" />,
+    url: 'https://mstdn.social/@jonrutter',
+  },
+];
