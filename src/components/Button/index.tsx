5f71270402506fa3e944930c6310d16fee4d8a87
Jon Rutter <contact@jonrutter.io>
2023-01-04 21:19:28 -0500

    Center button text

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,7 +16,7 @@ export const Button = <T extends React.ElementType = typeof Link>({
   return (
     <Tag
       {...rest}
-      className={`text-lg font-serif font-bold leading-none py-4 px-10 border-2 bg-transparent transition-colors ${
+      className={`text-lg font-serif font-bold leading-none py-4 px-10 border-2 bg-transparent transition-colors text-center ${
         variant === 'primary'
           ? 'border-pink-500 dark:border-pink-400 hover:bg-pink-400/30'
           : 'border-indigo-900 dark:border-white  hover:bg-indigo-900/10 dark:hover:bg-white/20'
