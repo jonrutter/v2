7c574141ce0e9d6651f696d079ab47c0a9f6f58e
Jon Rutter <contact@jonrutter.io>
2023-01-05 16:02:25 -0500

    Rename config to socialLinks

--- src/data/socialLinks.tsx
+++ src/data/socialLinks.tsx
@@ -11,25 +11,32 @@ type SocialLink = {
   url: string;
 };
 
+export const links = {
+  github: 'https://github.com/jonrutter',
+  linkedin: 'https://www.linkedin.com/in/rutterjt/',
+  codepen: 'https://codepen.io/jonrutter',
+  mastodon: 'https://mstdn.social/@jonrutter',
+};
+
 export const socialLinks: SocialLink[] = [
   {
     label: 'GitHub',
     icon: <GitHubIcon className="w-5 h-5" />,
-    url: 'https://github.com/jonrutter',
+    url: links.github,
   },
   {
     label: 'Linkedin',
     icon: <LinkedinIcon className="w-5 h-5" />,
-    url: 'https://www.linkedin.com/in/rutterjt/',
+    url: links.linkedin,
   },
   {
     label: 'CodePen',
     icon: <CodePenIcon className="w-5 h-5" />,
-    url: 'https://codepen.io/jonrutter',
+    url: links.codepen,
   },
   {
     label: 'Mastodon',
     icon: <MastodonIcon className="w-5 h-5" />,
-    url: 'https://mstdn.social/@jonrutter',
+    url: links.mastodon,
   },
 ];
