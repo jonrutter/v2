499fbaadb15610f6ee9b3681b57385edb4c9fbf0
Jon Rutter <contact@jonrutter.io>
2023-01-05 15:50:39 -0500

    Create custom render function
    Wrap react-testing-library's render function with the color mode
    context provider.

--- test/utils.tsx
+++ test/utils.tsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { ColorModeProvider } from '@/context/ColorMode';
+
+const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <ColorModeProvider>{children}</ColorModeProvider>
+);
+
+const customRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: Wrapper, ...options });
+
 export * from '@testing-library/react';
+export { customRender as render };
