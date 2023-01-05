51bb7ce6fa48bea818cf6f415c64b108c993f98f
Jon Rutter <contact@jonrutter.io>
2023-01-05 14:39:21 -0500

    Update imports

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '../../utils/test-utils';
+import { screen, render } from '@test/utils';
 import userEvent from '@testing-library/user-event';
 
 import { Button } from '.';
