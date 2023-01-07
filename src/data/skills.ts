f893adb2036cf222e68abe28b8083c42e40978c8
Jon Rutter <contact@jonrutter.io>
2023-01-06 20:26:53 -0500

    Add list of skills

---
+++ src/data/skills.ts
@@ -0,0 +1,33 @@
+type SkillType = {
+  label: string;
+  url: string;
+  color: string;
+};
+
+export const skills: SkillType[] = [
+  {
+    label: 'JavaScript (ES6)',
+    url: 'https://developer.mozilla.org/en-US/docs/Glossary/JavaScript',
+    color: '#F7DF1E',
+  },
+  {
+    label: 'TypeScript',
+    url: 'https://www.typescriptlang.org/',
+    color: '#3178C6',
+  },
+  { label: 'React', url: 'https://reactjs.org/', color: '#61DAFB' },
+  { label: 'Gatsby', url: 'https://www.gatsbyjs.com/', color: '#663399' },
+  { label: 'Tailwind CSS', url: 'https://tailwindcss.com/', color: '#38B2AC' },
+  { label: 'Redux', url: 'https://redux.js.org/', color: '#764ABC' },
+  { label: 'Material UI', url: 'https://mui.com/', color: '#0081CB' },
+  {
+    label: 'HTML',
+    url: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5',
+    color: '#E34F26',
+  },
+  {
+    label: 'CSS',
+    url: 'https://developer.mozilla.org/en-US/docs/Glossary/CSS',
+    color: '#1572B6',
+  },
+];
