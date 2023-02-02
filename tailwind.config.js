645132566e4f459c1490607dea1f933452c83324
Jon Rutter <contact@jonrutter.io>
2023-02-01 21:27:57 -0500

    Add grey color family to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,18 @@ module.exports = {
         800: '#0F0D3F',
         900: '#010024',
       },
+      grey: {
+        50: '#fafaff', // main bg: light
+        100: '#ececf9',
+        200: '#dbdbe1',
+        300: '#b0b0bf', // highlight text: dark
+        400: '#9a9aa7', // main body text: dark
+        500: '#65647d', // main body text: light
+        600: '#32314e', // highlight text: light
+        700: '#232334',
+        800: '#010024', // main bg: dark; heading text: dark
+        900: '#01001a', // loader bg
+      },
     },
     extend: {
       fontFamily: {
