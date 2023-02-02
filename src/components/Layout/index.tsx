f592df77647a46a4f0f7e1628e1447f7b6be47af
Jon Rutter <contact@jonrutter.io>
2023-02-02 18:30:28 -0500

    Ensure pages stretch to full height
    Style main layout as grid with main content taking up 1fr of row space.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,7 +8,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <div className="w-full min-w-screen max-w-screen overflow-x-hidden min-h-screen bg-grey-50 dark:bg-grey-800 text-grey-500 dark:text-grey-400 transition-colors">
+    <div className="w-full min-w-screen max-w-screen overflow-x-hidden min-h-screen bg-grey-50 dark:bg-grey-800 text-grey-500 dark:text-grey-400 transition-colors grid grid-cols-1 [grid-template-rows:_auto_1fr_auto]">
       <SkipToContent />
       <Header />
       <main className="px-6" id="main">
