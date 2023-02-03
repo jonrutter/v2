12da6bfebe589d938a9b0708e92fe7996bce59ad
Jon Rutter <contact@jonrutter.io>
2023-02-02 19:38:52 -0500

    Add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
+import { Layout } from '@/components/Layout';
+import { Landing } from '@/scenes/Landing';
+import { About } from '@/scenes/About';
+import { Portfolio } from '@/scenes/Portfolio';
+import { Contact } from '@/scenes/Contact';
 
 const HomePage: React.FC<PageProps> = () => {
-  return <h1>Hello, world!</h1>;
+  return (
+    <Layout>
+      <div id="home">
+        <Landing />
+      </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="portfolio">
+        <Portfolio />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </Layout>
+  );
 };
 
 export default HomePage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Jon Rutter | Web Developer</title>;
