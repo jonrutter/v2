f78fb71622a4ae75ccd8cc8a4be8fdaa4b7e8199
Jon Rutter <contact@jonrutter.io>
2023-02-15 19:48:24 -0500

    Create SkillButton component
    Extract skill button to separate component, since it is used in
    multiple places in the website.

---
+++ src/components/SkillButton/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import type { SkillType } from '@content/portfolio/types';
+
+const neutralColors = ['#000', '#000000', '#fff', '#ffffff'];
+
+export const SkillButton: React.FC<SkillType> = ({ url, color, label }) => {
+  const colorIsNeutral = neutralColors.indexOf(color) >= 0;
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+      className={`py-1 px-2 transition-opacity opacity-80 hover:opacity-100 focus:opacity-100 border inline-block ${
+        colorIsNeutral ? 'border-black dark:border-white' : ''
+      }`}
+      style={!colorIsNeutral ? { borderColor: color } : {}}
+    >
+      {label}
+    </a>
+  );
+};
