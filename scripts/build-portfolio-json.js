33d3d4a6234192397b278a48b33efa7fdeae4718
Jon Rutter <contact@jonrutter.io>
2023-02-20 02:13:20 -0500

    Remove unncessaary portfolio build infrastructure
    Remove portfolio json data, and build scripts, since now the
    portfolio items do not need to be sourced from graphql.

--- scripts/build-portfolio-json.js
+++
@@ -1,5 +0,0 @@
-/** A sub-script of ./build-portfolio.sh. Should not be run independently. */
-const fs = require('fs');
-const { portfolioItemsJson } = require('../content/portfolio/build/portfolio');
-
-fs.writeFileSync('./content/portfolio/portfolio.json', portfolioItemsJson);
