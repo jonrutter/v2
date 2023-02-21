3b4a7299b53bd5a6c21246d8885cefb23cdf6c16
Jon Rutter <contact@jonrutter.io>
2023-02-20 19:25:05 -0500

    Migrate tests to Next.js
    - Update jest config to use next/jest api
    
    - Move test files
    
    - Remove gatsby setup files and mocks

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,31 @@
-const { compilerOptions } = require('./tsconfig.json');
-const { pathsToModuleNameMapper } = require('ts-jest');
-const paths = pathsToModuleNameMapper(compilerOptions.paths, {
-  prefix: '<rootDir>/',
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
 });
 
-module.exports = {
-  transform: {
-    '^.+\\.[jt]sx?$': `<rootDir>/test/jest-preprocess.js`,
-  },
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+
+  // If you're using [Module Path Aliases](https://nextjs.org/docs/advanced-features/module-path-aliases),
+  // you will have to add the moduleNameMapper in order for jest to resolve your absolute paths.
+  // The paths have to be matching with the paths option within the compilerOptions in the tsconfig.json
+  // For example:
+
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/test/__mocks__/file-mock.js`,
-    ...paths,
+    '@/(.*)$': '<rootDir>/src/$1',
+    '@content/(.*)$': '<rootDir>/content/$1',
   },
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-  transformIgnorePatterns: [
-    `node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`,
-  ],
-  globals: {
-    __PATH_PREFIX__: ``,
-  },
-  testEnvironmentOptions: {
-    url: `http://localhost`,
-  },
-  testEnvironment: 'jsdom',
-  setupFiles: [`<rootDir>/test/loadershim.js`],
-  setupFilesAfterEnv: ['<rootDir>/test/setup-test-env.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
