d7dba2f6d673d0f322c1d835328d51293e1fd0f9
Jon Rutter <contact@jonrutter.io>
2023-02-22 17:49:23 -0500

    Add portfolio page
    - Move portfolio scene into own page
    
    - Fix headings

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { Portfolio } from '@/scenes/Portfolio';
+import { portfolioItems as pI } from '@/data/portfolio';
+import type { PortfolioItemType } from '@content/portfolio/types';
+import type { GetStaticProps, InferGetStaticPropsType } from 'next';
+
+const PortfolioPage = ({
+  portfolioItems,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <>
+      <Head>
+        <title>My Portfolio | Jon Rutter</title>
+        <meta
+          name="description"
+          content="Jon Rutter is a front-end engineer specializing in creating accessible, responsive, and performant websites and web applications with React."
+        />
+      </Head>
+      <Portfolio list={portfolioItems} />
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps<{
+  portfolioItems: PortfolioItemType[];
+}> = async () => {
+  const portfolioItems = pI.slice(0, 6);
+  return {
+    props: {
+      portfolioItems,
+    },
+  };
+};
+
+export default PortfolioPage;
