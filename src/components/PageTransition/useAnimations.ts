62f49bc1814e1d962b6fa86728b09c5d887a92e9
Jon Rutter <contact@jonrutter.io>
2023-02-23 18:06:37 -0500

    Fix ts error

--- src/components/PageTransition/useAnimations.ts
+++ src/components/PageTransition/useAnimations.ts
@@ -1,15 +1,11 @@
-import { useReducedMotion } from 'framer-motion';
 import type { Variants } from 'framer-motion';
 
 export const useAnimations = (): {
   main: Variants;
 } => {
-  const reduceMotion = useReducedMotion();
-
   const main: Variants = {
     hidden: {
       opacity: 0,
-      // x: reduceMotion ? 0 : -100,
       y: 0,
     },
     enter: {
@@ -25,7 +21,6 @@ export const useAnimations = (): {
     exit: {
       opacity: 0,
       x: 0,
-      // y: reduceMotion ? 0 : 50,
       transition: {
         duration: 0.2,
         type: 'keyframes',
