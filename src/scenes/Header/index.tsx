2d1ffa0842637acc6a2d870f87548a5d25ac1aa8
Jon Rutter <contact@jonrutter.io>
2023-02-24 19:01:13 -0500

    Update Header component
    - Use serif font for nav links
    
    - Make nav link font more legible
    
    - Remove unneeded fragment

--- src/scenes/Header/index.tsx
+++ src/scenes/Header/index.tsx
@@ -6,6 +6,7 @@ import { ColorModeSwitch } from '@/components/ColorModeSwitch';
 import { NavDialog } from './NavDialog';
 
 // TODO: add blog dropdown once blog functionality is added
+// TODO: add link underline animation that tracks with the current page
 
 /** Renders the site header */
 export const Header = () => {
@@ -16,27 +17,25 @@ export const Header = () => {
           <div>
             <Logo />
           </div>
-          <div className="flex justify-center items-center text-grey-700 dark:text-grey-200">
-            <>
-              <nav className="hidden md:flex justify-between items-center">
-                {/* site links */}
-                <ul
-                  className="flex justify-center items-center space-x-4 lg:space-x-6 font-semibold ml-20 text-base"
-                  key="site-links"
-                >
-                  {navLinks.map(({ label, url }) => (
-                    <li key={label}>
-                      <Link
-                        href={url}
-                        className="p-1 hover:text-grey-800 focus:text-grey-800 dark:hover:text-white dark:focus:text-white transition-colors"
-                      >
-                        {label}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </nav>
-            </>
+          <div className="flex justify-center items-center">
+            <nav className="hidden md:flex justify-between items-center">
+              {/* site links */}
+              <ul
+                className="flex justify-center items-center space-x-4 lg:space-x-6 font-serif font-light ml-20 text-lg text-grey-700 dark:text-grey-100"
+                key="site-links"
+              >
+                {navLinks.map(({ label, url }) => (
+                  <li key={label}>
+                    <Link
+                      href={url}
+                      className="p-1 hover:text-grey-800 focus:text-grey-800 dark:hover:text-white dark:focus:text-white transition-colors"
+                    >
+                      {label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
             <div className="flex items-center ml-20">
               <ColorModeSwitch />
             </div>
@@ -45,6 +44,7 @@ export const Header = () => {
             </div>
           </div>
         </div>
+        {/* decoration */}
         <div className="h-px absolute bottom-0 left-0 right-0 bg-gradient-to-r from-neon-400 to-pink-400" />
       </header>
       <div className="pt-20" />
