b6924d97a003ff5ebc39f9231fbf374eacf9f533
Jon Rutter <contact@jonrutter.io>
2023-02-24 19:02:29 -0500

    Improve menu button legibility

--- src/scenes/Header/NavDialog/NavDialogButton.tsx
+++ src/scenes/Header/NavDialog/NavDialogButton.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const NavDialogButton: React.FC<Props> = ({ onClick }) => (
   <button
-    className="text-grey-700 dark:text-grey-200 hover:text-grey-800 focus:text-grey-800 dark:hover:text-white dark:focus:text-white transition-colors px-2 font-serif font-semibold text-lg"
+    className="text-grey-700 dark:text-grey-200 hover:text-grey-800 focus:text-grey-800 dark:hover:text-white dark:focus:text-white transition-colors px-2 font-serif font-semibold text-lg tracking-wide"
     onClick={onClick}
     aria-label="open the navigation menu"
   >
