616815c38947ab675480bfe11c75dfab5154d0e3
Jon Rutter <contact@jonrutter.io>
2023-02-28 15:03:44 -0500

    Increase body text color contrast

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -11,7 +11,7 @@ type Props = {
 /** Renders all UI common to the site */
 export const Layout: React.FC<Props> = ({ route, children }) => {
   return (
-    <div className="w-full min-w-screen max-w-screen overflow-x-hidden min-h-screen bg-grey-50 dark:bg-grey-800 text-grey-500 dark:text-grey-400 transition-colors grid grid-cols-1 [grid-template-rows:_auto_1fr_auto]">
+    <div className="w-full min-w-screen max-w-screen overflow-x-hidden min-h-screen bg-grey-50 dark:bg-grey-800 text-grey-600 dark:text-grey-300 transition-colors grid grid-cols-1 [grid-template-rows:_auto_1fr_auto]">
       <SkipToContent />
       <Header route={route} />
       {children}
