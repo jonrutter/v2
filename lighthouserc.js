35bd3b19ea4f6f71ea2928f8e2aa0aaa620d0d3a
Jon Rutter <contact@jonrutter.io>
2023-03-01 19:48:15 -0500

    Update lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,16 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://127.0.0.1:3000/'],
+      url: ['http://localhost:3000'],
       startServerCommand: 'npm run start',
       startServerReadyPattern:
         'ready - started server on 0.0.0.0:3000, url: http://localhost:3000',
+      settings: {
+        preset: 'desktop',
+      },
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
     },
     upload: {
       target: 'temporary-public-storage',
