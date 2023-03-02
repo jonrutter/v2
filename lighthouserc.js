6836f78af3e94d654334fefe25722be85e811861
Jon Rutter <contact@jonrutter.io>
2023-03-01 19:24:13 -0500

    Configure lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      url: 'http://localhost:3000/',
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm start',
     },
     upload: {
       target: 'temporary-public-storage',
