f03edc31eb89c2dc1c624aec36a59257387b2b3c
Jon Rutter <contact@jonrutter.io>
2023-03-01 19:40:13 -0500

    Update ready pattern

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,8 @@ module.exports = {
     collect: {
       url: ['http://localhost:3000/'],
       startServerCommand: 'npm run start',
-      startServerReadyPattern: 'ready on',
+      startServerReadyPattern:
+        'ready - started server on 0.0.0.0:3000, url: http://localhost:3000',
     },
     upload: {
       target: 'temporary-public-storage',
